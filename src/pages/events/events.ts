e8a20ee19593e9beb9bf120a0c8a213ce8041460
Stephen Aldriedge <stephen@dactize.com>
2016-11-20 13:26:44 -0600

    events tab now shows events in list

--- src/pages/events/events.ts
+++ src/pages/events/events.ts
@@ -1,6 +1,8 @@
+import { WorldEvent } from './../../models/world-event';
+import { DataService } from './../../services/data.service';
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
 
 @Component({
   selector: 'page-events',
@@ -8,8 +10,26 @@ import { NavController } from 'ionic-angular';
 })
 export class EventsPage {
 
-  constructor(public navCtrl: NavController) {
+  worldEvents = []
 
+  constructor(public navCtrl: NavController, platform: Platform, private dataService: DataService) {
+      platform.ready().then(() => this.loadEvents());
   }
+
+  private loadEvents(){
+    this.dataService.obsv.subscribe(
+        worldEvents => {
+            this.worldEvents = worldEvents.map<any>(
+                    e => ({
+                      title: e.title,
+                      subtitle: e.source
+                    })
+            );
+        },
+        err => console.log(err),
+        () => console.log('done loading data')
+    );
+  }
+
 
 }
